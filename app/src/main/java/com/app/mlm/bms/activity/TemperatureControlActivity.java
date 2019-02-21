54fb33283a18a3af817b620f3bc54db491fe9b18
547558200@qq.com <ding17091952131>
2019-02-21 11:14:43 +0800

    1.温度配置页面UI

--- app/src/main/java/com/app/mlm/bms/activity/TemperatureControlActivity.java
+++ app/src/main/java/com/app/mlm/bms/activity/TemperatureControlActivity.java
@@ -1,14 +1,11 @@
 package com.app.mlm.bms.activity;
 
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.app.mlm.R;
-import com.app.mlm.application.MainApp;
 import com.app.mlm.bms.dialog.SigleChoiceDialog;
 import com.dinuscxj.progressbar.CircleProgressBar;
 
@@ -59,39 +56,39 @@ public class TemperatureControlActivity extends BaseActivity {
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        try {
-            //获取温度
-            int[] code = MainApp.bvmAidlInterface.BVMGetColdHeatTemp(1);
-            Log.e("箱内温度返回", "箱内温度" + code[0] + "箱外温度" + code[1]);// 箱内温度24箱外温度24
-            int code0 = MainApp.bvmAidlInterface.BVMSetColdHeatModel(1, 1);
-            Log.e("制冷制热模式设置", code0 + "");//99
-
-            int code1 = MainApp.bvmAidlInterface.BVMGetColdHeatModel(1);
-            Log.e("制冷制热模式查询", code1 + "");//1
-
-            int code2 = MainApp.bvmAidlInterface.BVMSetColdModel(1, 1);
-            Log.e("制冷模式设置", code2 + "");//99
-
-            int code3 = MainApp.bvmAidlInterface.BVMGetColdMode(1);
-            Log.e("制冷模式查询", code3 + "");//1
-
-            int code4 = MainApp.bvmAidlInterface.BVMSetColdTemp(1, 15, 5);
-            Log.e("制冷温度设置", code4 + "");//99
-
-            int[] code5 = MainApp.bvmAidlInterface.BVMGetColdTemp(1);
-            Log.e("制冷温度查询", "on" + code5[0] + "off" + code5[1]);//int[0]: 制冷ON温度 int[1] 制冷OFF温度
-
-            int code6 = MainApp.bvmAidlInterface.BVMSetHeatTemp(1, 10, 25);
-            Log.e("制热温度设置", code6 + "");//99
-
-            int[] code7 = MainApp.bvmAidlInterface.BVMGetHeatTemp(1);
-            Log.e("制热温度查询", "on" + code7[0] + "off" + code7[1]);// int[0]: 制冷ON温度 int[1] 制冷OFF温度
-
-            String version = MainApp.bvmAidlInterface.BVMGetVersion();
-            Log.e("中间层版本信息", version + "----");//中间层版本信息: null----
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            //获取温度
+//            int[] code = MainApp.bvmAidlInterface.BVMGetColdHeatTemp(1);
+//            Log.e("箱内温度返回", "箱内温度" + code[0] + "箱外温度" + code[1]);// 箱内温度24箱外温度24
+//            int code0 = MainApp.bvmAidlInterface.BVMSetColdHeatModel(1, 1);
+//            Log.e("制冷制热模式设置", code0 + "");//99
+//
+//            int code1 = MainApp.bvmAidlInterface.BVMGetColdHeatModel(1);
+//            Log.e("制冷制热模式查询", code1 + "");//1
+//
+//            int code2 = MainApp.bvmAidlInterface.BVMSetColdModel(1, 1);
+//            Log.e("制冷模式设置", code2 + "");//99
+//
+//            int code3 = MainApp.bvmAidlInterface.BVMGetColdMode(1);
+//            Log.e("制冷模式查询", code3 + "");//1
+//
+//            int code4 = MainApp.bvmAidlInterface.BVMSetColdTemp(1, 15, 5);
+//            Log.e("制冷温度设置", code4 + "");//99
+//
+//            int[] code5 = MainApp.bvmAidlInterface.BVMGetColdTemp(1);
+//            Log.e("制冷温度查询", "on" + code5[0] + "off" + code5[1]);//int[0]: 制冷ON温度 int[1] 制冷OFF温度
+//
+//            int code6 = MainApp.bvmAidlInterface.BVMSetHeatTemp(1, 10, 25);
+//            Log.e("制热温度设置", code6 + "");//99
+//
+//            int[] code7 = MainApp.bvmAidlInterface.BVMGetHeatTemp(1);
+//            Log.e("制热温度查询", "on" + code7[0] + "off" + code7[1]);// int[0]: 制冷ON温度 int[1] 制冷OFF温度
+//
+//            String version = MainApp.bvmAidlInterface.BVMGetVersion();
+//            Log.e("中间层版本信息", version + "----");//中间层版本信息: null----
+//        } catch (RemoteException e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
