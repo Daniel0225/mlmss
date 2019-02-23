e4d852a1c28cca4ee9dfd12b5520d2234d11c1b2
547558200@qq.com <ding17091952131>
2019-02-23 19:22:44 +0800

    取货失败页面

--- app/src/main/java/com/app/mlm/fragment/ChuhuoFailedFragment.java
+++ app/src/main/java/com/app/mlm/fragment/ChuhuoFailedFragment.java
@@ -2,30 +2,25 @@ package com.app.mlm.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.widget.TextView;
 
 import com.app.mlm.R;
 
-import butterknife.Bind;
-
 /**
+ * 出货失败Fragment
  * A simple {@link Fragment} subclass.
  */
-public class ChuhuoFragment extends BaseFragment {
-    @Bind(R.id.tvCountDown)
-    TextView tvCountDownView;
-
-    public ChuhuoFragment() {
+public class ChuhuoFailedFragment extends BaseFragment {
+    public ChuhuoFailedFragment() {
     }
 
     @Override
     protected int provideLayoutResId() {
-        return R.layout.fragment_chuhuo;
+        return R.layout.fragment_chuhuo_failed;
     }
 
     @Override
     protected void initView(Bundle savedInstanceState) {
-        initCountDownMananger();
+
     }
 
     @Override
@@ -45,10 +40,7 @@ public class ChuhuoFragment extends BaseFragment {
 
     @Override
     public void onTick(int seconds) {
-        tvCountDownView.setText(seconds + "S");
-        if (seconds == 55) {
-            mActivity.addFragment(new ChuhuoFailedFragment());
-        }
+
     }
 
     @Override
