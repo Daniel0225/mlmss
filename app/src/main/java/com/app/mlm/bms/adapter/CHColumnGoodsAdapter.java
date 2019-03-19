f480a2cada0bb6f18ca54eb52eeb242319b00d54
547558200@qq.com <ding17091952131>
2019-03-19 16:15:10 +0800

    出货测试位置转换

--- app/src/main/java/com/app/mlm/bms/adapter/CHColumnGoodsAdapter.java
+++ app/src/main/java/com/app/mlm/bms/adapter/CHColumnGoodsAdapter.java
@@ -48,7 +48,8 @@ public class CHColumnGoodsAdapter extends RecyclerView.Adapter<CHColumnGoodsAdap
         LinearLayoutManager ms = new LinearLayoutManager(context);
         ms.setOrientation(LinearLayoutManager.HORIZONTAL);
         rowGoodsViewHolder.recyclerView.setLayoutManager(ms);
-        CHRowGoodsAdapter adapter = new CHRowGoodsAdapter(context, data.get(i), i);
+        int position = data.size() - i;//注意机器货道最下一层是1
+        CHRowGoodsAdapter adapter = new CHRowGoodsAdapter(context, data.get(i), position);
         rowGoodsViewHolder.recyclerView.setAdapter(adapter);
         rowGoodsViewHolder.recyclerView.addItemDecoration(new SpacesItemDecoration(0, 0, 0, 2));
         rowGoodsViewHolder.tvColumn.setText(String.valueOf(i + 1));
