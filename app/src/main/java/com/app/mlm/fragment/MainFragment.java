f40a63712b23bd25a1678bbfc8addf90b3cc8b64
mervin <21194392@qq.com>
2019-03-30 22:44:52 +0800

    *删除手机运行参数

--- app/src/main/java/com/app/mlm/fragment/MainFragment.java
+++ app/src/main/java/com/app/mlm/fragment/MainFragment.java
@@ -1,6 +1,5 @@
 package com.app.mlm.fragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -19,8 +18,6 @@ import com.app.mlm.application.MainApp;
 import com.app.mlm.bean.AddShopCarEvent;
 import com.app.mlm.bean.ChuhuoSuccessBean;
 import com.app.mlm.bean.GoodsInfo;
-import com.app.mlm.bms.activity.BackgroundManangerSystemActivity;
-import com.app.mlm.bms.dialog.CommonDialog;
 import com.app.mlm.bms.dialog.ShopCarDialog;
 import com.app.mlm.http.bean.HuodaoBean;
 import com.app.mlm.utils.FastJsonUtil;
@@ -153,14 +150,14 @@ public class MainFragment extends BaseFragment {
                 break;
             case R.id.huodong:
                 Toast.makeText(getActivity(), "敬请期待", Toast.LENGTH_SHORT).show();
-                CommonDialog commonDialog = new CommonDialog(getActivity(), "系统维护", "请确认是否维护售货机", "确定", "取消");
+       /*         CommonDialog commonDialog = new CommonDialog(getActivity(), "系统维护", "请确认是否维护售货机", "确定", "取消");
                 commonDialog.setCommitClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         startActivity(new Intent(getActivity(), BackgroundManangerSystemActivity.class));
                     }
                 });
-                commonDialog.show();
+                commonDialog.show();*/
                 break;
         }
     }
